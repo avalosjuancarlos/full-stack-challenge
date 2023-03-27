202666a72ec33870a174cc64ed73a620486d3dbe
Juan Carlos Avalos <juancarlosavalos@Juans-MacBook-Air.local>
2023-03-27 19:58:35 -0300

    add try and catch and return array

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -2,8 +2,15 @@ const API_URL = '';
 
 // Load all files data
 async function httpGetAllFilesData() {
+    try {
     const response = await fetch(`${API_URL}/files/data`);
-    return await response.json();
+    const data = await response.json();
+
+    //Data to parse
+    return data;
+    } catch(err){
+        return [];
+    }
 }
 
 export {
