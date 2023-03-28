03c4d36888d59875dfcc19b1d89830258f1ac53c
Juan Carlos Avalos <juancarlosavalos@Juans-MacBook-Air.local>
2023-03-28 16:29:42 -0300

    add nginx, docker and docker-compose

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # full-stack-challenge
 [v3] Full Stack JS - Code Challenge - Español
+
+Run `npm i` inside the client directory
+
+Run `npm i` inside the server directory
+
+Run `docker-compose up --build` inside the mainn project directory
+
+To start interacting with the application, open `http://localhost:3050/` on a browser.
--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,14 +1,14 @@
-//const API_URL = '';
-const API_URL = 'http://localhost:3001';
+const API_URL = '/api';
+//const API_URL = 'http://localhost:3001';
+
 // Load all files data
 async function httpGetAllFilesData() {
     try {
-    const response = await fetch(`${API_URL}/files/data`);
-    const data = await response.json();
-
-    //Data to parse
-    return data;
-    } catch(err){
+        const response = await fetch(`${API_URL}/files/data`);
+        const data = await response.json();
+        return data;
+    } catch (err) {
+        console.error(err);
         return [];
     }
 }
